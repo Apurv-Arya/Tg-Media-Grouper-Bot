1ddba8a780fdd1255558a25a135eac323ac06f43
Apurv Arya <apurvarya19@gmail.com>
2025-08-07 15:12:38 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 from os import getenv
 
-# --- Telegram Bot Configuration ---
-# Get the bot token from the environment variable.
-# The script will raise an error if the token is not found.
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "7080897951:AAEX_VM9sxtKSG0B7mSCWic7KOGsVT6PNGE")
 
 if not BOT_TOKEN:
     raise ValueError("No BOT_TOKEN found. Please set it in your .env file.")
