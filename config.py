bb9488f5e92b53c54df1fbc9407e85dbed50fa29
Apurv Arya <94875571+Apurv-Arya@users.noreply.github.com>
2025-08-23 22:51:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7080897951:AAEX_VM9sxtKSG0B7mSCWic7KOGsVT6PNGE")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 if not BOT_TOKEN:
     raise ValueError("No BOT_TOKEN found. Please set it in your .env file.")
